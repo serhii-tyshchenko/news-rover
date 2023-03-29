c7e39c05c89fe91ca523c7943d058641c3b22e15
Serhii Tyshchenko <serhii.tyshchenko84@gmail.com>
2023-03-29 18:56:43 +0300

    fixed time display

--- src/components/organisms/widget/Widget.utils.ts
+++ src/components/organisms/widget/Widget.utils.ts
@@ -31,7 +31,7 @@ export async function fetchNews(
 
 export const convertDate = (date: Date) => {
   const rawDate = new Date(date);
-  const hours = rawDate.getHours();
+  const hours = rawDate.getHours().toString().padStart(2, '0');
   const minutes = rawDate.getMinutes().toString().padStart(2, '0');
   return `${hours}:${minutes}`;
 };
