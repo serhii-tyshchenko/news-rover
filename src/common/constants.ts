4d06430bdc31addcf6ac146dce6d977c750e7edd
Serhii Tyshchenko <serhii.tyshchenko84@gmail.com>
2023-03-31 00:49:11 +0300

    removed unused values

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,50 +1,3 @@
-export const PROVIDERS = {
-  CENSOR: {
-    name: 'censor',
-    title: 'Цензор',
-  },
-  LB: {
-    name: 'lb',
-    title: 'Lb.ua',
-  },
-  PRAVDA: {
-    name: 'pravda',
-    title: 'Українська правда',
-  },
-  NV: {
-    name: 'nv',
-    title: 'НВ',
-  },
-  ESPRESO: {
-    name: 'espreso',
-    title: 'Еспресо',
-  },
-  MILITARNYJ: {
-    name: 'militarnyj',
-    title: 'Мілітарний портал',
-  },
-  DEFENCE_UA: {
-    name: 'defense_express',
-    title: 'Defense Express',
-  },
-  GAZETA_UA: {
-    name: 'gazeta',
-    title: 'Газета',
-  },
-  UNIAN: {
-    name: 'unian',
-    title: 'УНІАН',
-  },
-  OBOZREVATEL: {
-    name: 'obozrevatel',
-    title: 'Обозреватель',
-  },
-  ARMYINFORM: {
-    name: 'armyinform',
-    title: 'Армія Інформ',
-  },
-};
-
 export const DEFAULT_POSTS_LIMIT = 10;
 
 export const APP_NAME = 'MyNews';
@@ -55,3 +8,5 @@ export const THEMES = {
   LIGHT: 'light',
   DARK: 'dark',
 };
+
+export const portalRoot = document.getElementById('portal-root') as HTMLElement;
