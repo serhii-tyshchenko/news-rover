7e7961bd5cd2d2e06bb641332185a83ca7ea6c01
Serhii Tyshchenko <serhii.tyshchenko84@gmail.com>
2024-01-26 13:27:59 +0200

    tweaked layout

--- src/components/skeleton/skeleton.tsx
+++ src/components/skeleton/skeleton.tsx
@@ -11,7 +11,7 @@ function Skeleton() {
   });
 
   return (
-    <ul>
+    <ul className="d-flex flex-direction-column h-100 overflow-y-auto pr-1">
       {Array.from({ length: DEFAULT_POSTS_LIMIT }).map((_, index) => (
         <li className={className} key={index} />
       ))}
