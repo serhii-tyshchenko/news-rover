2d6407b5929ba2ae4bdf4dff9c916c441b2cb0b8
Serhii Tyshchenko <serhii.tyshchenko84@gmail.com>
2024-05-22 18:27:30 +0300

    added thumbnail to news item

--- src/core/api.ts
+++ src/core/api.ts
@@ -2,6 +2,15 @@ import { DEFAULT_POSTS_LIMIT } from '@constants';
 
 import { PROVIDERS_ROOT_URL, NEWS_ROOT_URL } from './api.contants';
 
+const formatResponse = (data: any[]) => {
+  return data.map((item) => ({
+    created: item.created,
+    title: item.title,
+    link: item.link,
+    thumbnail: item?.enclosures[0]?.url || null,
+  }));
+};
+
 export const getProviders = async () => {
   try {
     const response = await fetch(PROVIDERS_ROOT_URL);
@@ -21,7 +30,7 @@ export const getNewsByProvider = async (
       `${NEWS_ROOT_URL}?provider=${provider}&limit=${limit}`,
     );
     const data = await response.json();
-    return data;
+    return formatResponse(data);
   } catch (error) {
     throw error;
   }
