88e69e76e33fa5f7fb37a992c7a9bdbd23809c44
Serhii Tyshchenko <serhii.tyshchenko84@gmail.com>
2026-04-22 00:42:12 +0300

    refactor: remove use-dialog-state hook and associated tests

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -1,6 +1,5 @@
 export { default as useAnimation } from './use-animation';
 export * from './use-breakpoints';
-export * from './use-dialog-state';
 export * from './use-draggable-list';
 export * from './use-fullscreen';
 export { default as useLocalization } from './use-localization';
